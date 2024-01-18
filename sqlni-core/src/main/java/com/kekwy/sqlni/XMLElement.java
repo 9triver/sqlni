107356dbe9c71b752b00b7151cb4ab66f2aaa21c
Kekwy <keekkewy@qq.com>
2024-01-18 09:15:46 +0800

    feat: XMLElement

--- sqlni-core/src/main/java/com/kekwy/sqlni/XMLElement.java
+++ sqlni-core/src/main/java/com/kekwy/sqlni/XMLElement.java
@@ -3,7 +3,7 @@ package com.kekwy.sqlni;
 import java.util.*;
 
 /**
- * description
+ * 保存由 SQLNI 语句转化得到的 XML 数据及其结构
  *
  * @author Kekwy
  * @version 1.0
@@ -19,12 +19,29 @@ public class XMLElement {
         NODE
     }
 
+    /**
+     * 当前元素的类型，
+     * - TEXT: 文本
+     * - NODE: 结点
+     */
     private Type type;
 
+    /**
+     * 为文本时，value 为 String 对象
+     * 为结点时，value 为 Object[] 对象，
+     * 其中：
+     * - objects[0]: String name;
+     * - objects[1]: Map attributes;
+     * - objects[2]: List elements;
+     */
     private Object value;
 
     public boolean isText() {
         return type == Type.TEXT;
+    }
+
+    public boolean isNode() {
+        return type == Type.NODE;
     }
 
     public String getText() {
@@ -32,8 +49,16 @@ public class XMLElement {
         else throw new RuntimeException("XMLElement 类型错误，不是 TEXT");
     }
 
-    public boolean isNode() {
-        return type == Type.NODE;
+    public String getName() {
+        if (isNode())
+            return (String) ((Object[]) value)[0];
+        else throw new RuntimeException("XMLElement 类型错误，不是 NODE");
+    }
+
+    public Map<String, String> getAttributes() {
+        if (isNode()) //noinspection unchecked
+            return (Map<String, String>) ((Object[]) value)[1];
+        else throw new RuntimeException("XMLElement 类型错误，不是 NODE");
     }
 
     public List<XMLElement> getElements() {
@@ -41,7 +66,6 @@ public class XMLElement {
             return (List<XMLElement>) ((Object[]) value)[2];
         else throw new RuntimeException("XMLElement 类型错误，不是 NODE");
     }
-
 
     public static XMLElement createText(String text) {
         XMLElement element = new XMLElement();
@@ -51,12 +75,21 @@ public class XMLElement {
     }
 
     public static XMLElement createNode(String name, Map<String, String> attributes) {
+        Object[] value = new Object[3];
+        value[0] = name;
+        value[1] = new HashMap<>(attributes);
+        value[2] = new LinkedList<XMLElement>();
+
         XMLElement element = new XMLElement();
         element.type = Type.NODE;
-//        Map<String, String> attributes = new HashMap<>();
-        List<XMLElement> elements = new LinkedList<>();
-        element.value = new Object[]{attributes, elements};
+        element.value = value;
+
         return element;
+    }
+
+    public void addElement(XMLElement element) {
+        List<XMLElement> elements = getElements();
+        elements.add(element);
     }
 
 }
---
+++ sqlni-core/src/test/java/com/kekwy/sqlni/test/ObjectArrayTest.java
@@ -0,0 +1,20 @@
+package com.kekwy.sqlni.test;
+
+import org.junit.Test;
+
+/**
+ * description
+ *
+ * @author Kekwy
+ * @version 1.0
+ * @since 2024/1/18 8:26
+ */
+public class ObjectArrayTest {
+
+    @Test
+    public void test() {
+        Object[] objects = new Object[3];
+        System.out.println(objects[2]);
+    }
+
+}
