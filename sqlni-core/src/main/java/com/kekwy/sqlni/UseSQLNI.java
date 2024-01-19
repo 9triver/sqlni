45fc875fefb3e339573c677564e2e3cf98feac4a
Kekwy <keekkewy@qq.com>
2024-01-19 10:03:48 +0800

    feat: 完善 UseSQLNI 注解类的实现

--- sqlni-core/src/main/java/com/kekwy/sqlni/UseSQLNI.java
+++ sqlni-core/src/main/java/com/kekwy/sqlni/UseSQLNI.java
@@ -1,12 +1,21 @@
 package com.kekwy.sqlni;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
- * description
+ * 在 Mapper 的方法上添加该注解，并编写 SQLNI 语句。
+ * 在项目编译时，将会根据用户编写的 SQLNI 语句以及指定的数据库类型，
+ * 自动生成 Mapper 对应的 XML 映射文件。
  *
  * @author Kekwy
  * @version 1.0
  * @since 2024/1/11 16:36
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
 public @interface UseSQLNI {
 
     String value();
