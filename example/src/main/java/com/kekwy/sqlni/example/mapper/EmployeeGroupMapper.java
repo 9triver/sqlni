1c9c5b6c6aef70fe0262b37a5cb8794e94dfda15
Kekwy <keekkewy@qq.com>
2024-05-27 10:30:46 +0800

    tmp

--- example/src/main/java/com/kekwy/sqlni/example/mapper/EmployeeGroupMapper.java
+++ example/src/main/java/com/kekwy/sqlni/example/mapper/EmployeeGroupMapper.java
@@ -2,6 +2,35 @@ package com.kekwy.sqlni.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kekwy.sqlni.example.entity.EmployeeGroup;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
 
-public interface EmployeeGroupMapper extends BaseMapper<EmployeeGroup> {
+import java.util.List;
+
+@Mapper
+public interface EmployeeGroupMapper {
+
+    @Results(
+            id = "res1",
+            value = {
+            @Result(property = "gid", column = "gid"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "members", column = "gid",
+                    many = @Many(
+                            select = "com.kekwy.sqlni.example.mapper.EmployeeMapper.getEmployeeByGid"
+                    ))}
+    )
+    @Select("""
+            select * from t_emp_g where gid=#{gid};
+            """)
+    EmployeeGroup getEmployeeGroupByGid(Integer gid);
+
+    @ResultMap("res1")
+    @Select("""
+            select * from t_emp_g where gid in ( #foreach(#{gidList}) );
+            """)
+    List<EmployeeGroup> getEmployeeGroupsByGid(List<Integer> gidList);
+
 }
+
+
--- example/src/main/java/com/kekwy/sqlni/example/mapper/EmployeeMapper.java
+++ example/src/main/java/com/kekwy/sqlni/example/mapper/EmployeeMapper.java
@@ -12,46 +12,60 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
 
-//    @Select("""
-//            select * from t_employee where eid = #{eid};
-//            """)
-//    Employee getEmployeeByEid(Integer eid);
-//
-//    @Select("""
-//            select * from t_employee limit #{l} offset #{o}
-//            """)
-//    List<Employee> getEmployeeWithPage(int l, int o);
-//
-//
-//    /**
-//     * 选出指定某些部门中工资最高的员工的姓名
-//     */
-//    @Select("""
-//            select employName
-//            from t_employee
-//            where (department, salary) in (
-//                select department, max(salary) max_salary
-//                from t_employee
-//                where 1=1 #if(departmentSet!=null) department in #foreach(#{departmentSet}) #endif
-//                group by department
-//            );
-//            """)
-//    List<Employee> getEmployeeWithSubQuery(List<String> departmentSet);
-//
-//    /**
-//     * 用包含对象的属性值的集合作为方法的参数用以创建对象是一种很糟糕的做法，此处仅用于测试。
-//     */
-//    @Insert("""
-//            insert into t_employ values (#foreach(#{attributes}));
-//            """)
-//    boolean addEmployee(List<Object> attributes);
+    @Select("""
+            select * from t_employee where eid = #{eid};
+            """)
+    Employee getEmployeeByEid(Integer eid);
+
+    @Select("""
+            select * from t_employee where gid = #{gid};
+            """)
+    Employee getEmployeeByGid(Integer gid);
+
+    @Select("""
+            select * from t_employee limit #{l}
+            """)
+    List<Employee> getEmployeeWithPage(int l, int o);
+
+
+    @Select("""
+            select * from t_employee where eid in ( #foreach(#{eid}) );
+            """)
+    List<Employee> getEmployeesByEid(List<Integer> eidList);
+
+
+    /**
+     * 选出指定的某些部门中工资最高的员工的姓名，
+     * 选取其中工资最高的十名员工的部门和姓名并将其拼接。
+     */
+    @Select("""
+            select concat(dept, '_', name) dept_name
+            from t_employee
+            where (dept, sal) in (
+                select dept, max(sal) max_sal
+                from t_employee
+                where 1 = 1 #if(deptSet!=null) and dept in #foreach(#{deptSet}) #endif
+                group by dept
+            )
+            order by sal
+            limit 10;
+            """)
+    List<Employee> getEmployeeWithSubQuery(List<String> deptSet);
+
+    /**
+     * 用包含对象的属性值的集合作为方法的参数用以创建对象是一种很糟糕的做法，此处仅用于测试。
+     */
+    @Insert("""
+            insert into t_employ values (#foreach(#{attributes}));
+            """)
+    boolean addEmployee(List<Object> attributes);
 
     @Update("""
             update t_employ
             set #if(employee.eid!=null) eid=#{employee.eid} #endif
             #if(employee.gid!=null) eid=#{employee.gid} #endif
             #if(employee.employeeName!=null) eid=#{employee.employeeName} #endif
-            #if(employee.department!=null) eid=#{employee.department} #endif
+            #if(employee.department!=null) eid=concat(#{employee.department}, '_', #{employee.name}) #endif
             #if(employee.salary!=null) eid=#{employee.salary} #endif
             where eid = #{eid};
             """)
